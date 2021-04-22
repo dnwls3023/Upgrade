65d026a4f08312ec749ee0d82f3a48630617dace
dnwls3023 <68366527+dnwls3023@users.noreply.github.com>
2021-04-22 23:56:44 +0900

    Update upgrade.cpp

--- upgrade.cpp
+++ upgrade.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <random>
 using namespace std;
 
-// 강화를 위주로 구현 (미완)
+// 강화를 위주로 구현
 
 class ItemHandler {
-
+private:
+	vector<Item> Items;
 };
 
 class Item {
@@ -15,7 +18,17 @@ private:
 public:
 	Item(string _name, string _desc,int _type) :
 		name(_name),desc(_desc),type(_type) {}
+
+	void print() {
+		string str[3] = { "","무기","장비" };
+		
+		cout << "아이템 이름 : " << name << endl;
+		cout << "아이템 종류 : " << str[type] << endl;
+		cout << "아이템 설명 : " << desc << endl;
+	}
 };
+
+int money;
 
 class Weapon : public Item{
 private:
@@ -25,20 +38,58 @@ private:
 	const int maxStar = 10;
 	// 강화 확률
 	const int chance[10] = { 100,95,90,85,80,75,70,65,60,55 };
+	// 강화 비용
+	const int cost[10] = { 100,200,400,800,1500,3000,6000,10000,15000,30000 };
+	// 데미지 증가량
+	const int increaseDamage[10] = { 2,3,5,10,13,16,19,25,30,40 };
+
+	// 기본 무기 데미지
+	const int default_damage;
 	// 무기 데미지
 	int damage;
 public:
 	Weapon(string _name, string _desc, int _type, int _damage) 
-		: Item(_name,_desc,_type),damage(_damage) {}
+		: Item(_name,_desc,_type),damage(_damage),default_damage(_damage) ,star(0) {}
 
-
+	void print() {
+		Item::print();
+		for (int i = 0; i < 30; ++i) cout << '-';
+		cout << star << "성 아이템" << endl;
+		cout << "데미지 : " << default_damage << "(+" << damage - default_damage << ")" << endl;
+	}
 
 	void Upgrade() {
-		
+		if (star == maxStar) {
+			cout << "이미 최대 강화 수치이므로 강화가 불가능 합니다." << endl;
+		}
+
+		random_device rd;
+		mt19937 gen(rd());
+		uniform_int_distribution<int> dis(1, 100);
+
+		if (money >= cost[star]) money -= cost[star];
+		else {
+			cout << "돈이 부족합니다." << endl;
+			return;
+		}
+
+		if (chance[star] - dis(gen) >= 0) {
+			cout << "강화 성공!!" << endl;
+			damage += increaseDamage[star];
+			star++;
+		}
+		else {
+			cout << "강화 실패!" << endl;
+		}
 	}
 };
 
 class Equipment : public Item {
+private:
+public:
+};
+
+class Player {
 private:
 public:
 };
